5e8f258ec014b32d5f40b38461dc33db0ca8e199
Sukhdeep Singh <sukh.butalia@gmail.com>
2022-04-29 11:27:35 +0530

    Setup project skeleton

--- src/index.js
+++ src/index.js
@@ -1,11 +1,3 @@
-// import './index.css';
+import './index.css';
 
-// console.log('ਨਿਰਵਾਣ ਬੱਲ نِروَاݨ بلّ');
-
-// const body = document.querySelector('body');
-// const heading = document.createElement('h1');
-// heading.textContent = 'Battleship';
-// body.appendChild(heading);
-
-export const sum = (a, b) => a + b;
-export const mul = (a, b) => a * b;
+console.log('ਨਿਰਵਾਣ ਬੱਲ نِروَاݨ بلّ');
