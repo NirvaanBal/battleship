bebfe1c93b9ab057b0639255985335f2a4667c7d
Sukhdeep Singh <sukh.butalia@gmail.com>
2022-05-01 16:43:45 +0530

    Remove human from factory

--- src/player/Player.js
+++ src/player/Player.js
@@ -1,28 +1,17 @@
-import updateRowToNumeral from '../utils/updateRowToNumeral';
-
 const Player = (player = 'human') => {
   const cachedLocations = [];
 
-  const whoWillMove = player;
+  const move = () => {
+    const row = Math.floor(Math.random() * 10);
+    const col = Math.floor(Math.random() * 10);
 
-  const move = (row, col) => {
-    if (player === 'computer') {
-      if (cachedLocations.includes(`${row}${col}`)) return false;
+    if (cachedLocations.includes(`${row}${col}`)) return false;
 
-      row = Math.floor(Math.random() * 10);
-      col = Math.floor(Math.random() * 10);
-
-      cachedLocations.push(`${row}${col}`);
-      return `${row}${col}`;
-    }
-
-    row = updateRowToNumeral(row);
-    col -= 1;
-
+    cachedLocations.push(`${row}${col}`);
     return `${row}${col}`;
   };
 
-  return { move, whoWillMove };
+  return { move, cachedLocations, player };
 };
 
 export default Player;
