d37cc4163841d96bea6865c26088e5bbe2f8f147
Sukhdeep Singh <sukh.butalia@gmail.com>
2022-05-01 10:38:41 +0530

    Style gameboard grid

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import './index.css';
+import Game from './game/Game';
+import gameboardHTML from './dom/gameboardHTML';
 
-console.log('ਨਿਰਵਾਣ ਬੱਲ نِروَاݨ بلّ');
+const root = document.getElementById('root');
+const heading = document.createElement('h1');
+heading.textContent = 'Battleship';
+root.appendChild(heading);
+
+const computerBoard = Game().gameboardC;
+
+root.insertAdjacentHTML('beforeend', gameboardHTML(computerBoard.grid));
+
+// console.log('ਨਿਰਵਾਣ ਬੱਲ نِروَاݨ بلّ');
